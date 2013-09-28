e4e87c562fedc7945af3144abc83dcea2b96aa1a
ODE Lab <ode.it@umich.edu>
2013-09-28 18:46:54 -0400

    changed so it doesnt run MAP for setting initial MCMC values, the alternative is to return all named objects in MCMC_model explictily rather that return_locals

--- MMSB_mcmc/MMSB_run.py
+++ MMSB_mcmc/MMSB_run.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 
 
 #---------------------------- Load Data --------------------------------------#
-data_matrix=np.loadtxt("../data/Y_alpha0.1_K5_N20.txt")
+data_matrix=np.loadtxt("../data/Y_alpha0.1_K5_N20.txt", delimiter=',')
 num_people = 20
 num_groups = 5
 alpha = np.ones(num_groups).ravel()*0.1
@@ -28,12 +28,13 @@ B = B + np.ones([num_groups,num_groups])*0.2-np.eye(num_groups)*0.2
 
 #---------------------------- Setup Model -----------------------------------#
 raw_model = model.create_model(data_matrix, num_people, num_groups, alpha, B)
-MC_model = pymc.Model(raw_model)
+#model_instance = pymc.Model(raw_model)
+
 #---------------------------- Call MAP to initialize MCMC -------------------#
-pymc.MAP(model).fit(method='fmin_powell')
+#pymc.MAP(model_instance).fit(method='fmin_powell')
 print '---------- Finished Running MAP to Set MCMC Initial Values ----------'
 #---------------------------- Run MCMC --------------------------------------#
 print '--------------------------- Starting MCMC ---------------------------'
-M = pymc.MCMC(model)
-M.sample(10000,5000, thin=5, verbose=0)
+M = pymc.MCMC(raw_model)
+M.sample(100000,50000, thin=5, verbose=0)
 
